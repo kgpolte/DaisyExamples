5c4fce4c5224533e892dfa971c6821a5fa9d69e6
kgpolte <120602839+kgpolte@users.noreply.github.com>
2024-02-23 09:25:53 -0800

    Update DrumDrum.cpp
    Read kick trigger input. Use onboard LED to test.

--- MyProjects/DrumDrum/DrumDrum.cpp
+++ MyProjects/DrumDrum/DrumDrum.cpp
@@ -2,6 +2,7 @@
 #include "daisysp.h"
 
 using namespace daisy;
+using namespace daisy::seed;
 using namespace daisysp;
 
 // -------------------------------------------------------------
@@ -15,27 +16,27 @@ WhiteNoise noise;
 
 AdEnv kickVolEnv, kickPitchEnv, snareEnv;
 
-Switch kickTrig, snareTrig;
+Switch btn;
+GPIO kickTrig, snareTrig;
 
 // -------------------------------------------------------------
 //
 // -------------------------------------------------------------
 
-#define FREQ_PIN 			15  // Seed Pin 22
-#define DECAY_PIN 			16  // Seed Pin 23
-#define FM_PIN				17  // Seed Pin 24
-#define DRIVE_PIN			18  // Seed Pin 25
-#define FREQ_CV_PIN			24  // Seed Pin 31
-#define DECAY_CV_PIN		25  // Seed Pin 32
-#define KICK_TRIG_PIN		7   // Seed Pin 8
-#define SNARE_TRIG_PIN		8   // Seed Pin 9
-#define BTN_PIN             9  // Seed Pin 10
+#define FREQ_PIN 			15              // Seed Pin 22
+#define DECAY_PIN 			16              // Seed Pin 23
+#define FM_PIN				17              // Seed Pin 24
+#define DRIVE_PIN			18              // Seed Pin 25
+#define FREQ_CV_PIN			24              // Seed Pin 31
+#define DECAY_CV_PIN		25              // Seed Pin 32
+
+#define BTN_PIN             9               // Seed Pin 10
 
 // -------------------------------------------------------------
 //
 // -------------------------------------------------------------
 
-// Remember to update this when adding/removing ranged controls
+// Remember to update this when adding/removing analog controls
 int numAdcChannels = 5;
 
 // Kick drum variables
@@ -45,6 +46,9 @@ float kickMinFm = 50;
 float kickMaxFm = 300;
 float kickMinDecay = 0.2;
 float kickMaxDecay = 10;
+
+bool lastState = false;
+bool risingEdge = false;
 
 // -------------------------------------------------------------
 //
@@ -57,10 +61,20 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     float osc_low_out, noise_out, snr_env_out, kck_env_out, kick_sig;
 
     //Get rid of any bouncing
-    snareTrig.Debounce();
-    kickTrig.Debounce();
+    btn.Debounce();
 
-    if(kickTrig.RisingEdge())
+    // Trigger inputs are inverted
+    // Gate high on the input results in pin reading low (false)
+    bool state = kickTrig.Read();
+    if (state == false && lastState == true) {
+        lastState = false;
+        hw.SetLed(true);
+    } else if (state == true && lastState == false) {
+        lastState = true;
+        hw.SetLed(false);
+    }
+
+    if (btn.RisingEdge())
     {
 		// Read ADC inputs
 		float freqPot = hw.adc.GetFloat(0);
@@ -69,9 +83,10 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
 		float decayCV = (hw.adc.GetFloat(5) * (kickMaxDecay - kickMinDecay)) + kickMinDecay;
 		float decayManual = (hw.adc.GetFloat(1) * (kickMaxDecay - kickMinDecay)) + kickMinDecay;
 
+        // Set the volume decay time
 		kickVolEnv.SetTime(ADENV_SEG_DECAY, decayManual);
 
-		//Calculate min and max freqencies for the pitch envelope
+		// Calculate min and max freqencies for the pitch envelope
 		float freqMin = kickFreqMin + (freqPot * (kickFreqMax - kickFreqMin));
 		float fmAmt = kickMinFm + (fmPot * (kickMaxFm - kickMinFm));
 		float freqMax = freqMin + fmAmt;
@@ -83,11 +98,6 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
         // Trigger the envelopes
         kickVolEnv.Trigger();
         kickPitchEnv.Trigger();
-    }
-
-    if(snareTrig.RisingEdge())
-    {
-        snareEnv.Trigger();
     }
 
     //Prepare the audio block
@@ -103,6 +113,8 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
         osc_low.SetAmp(kck_env_out);
         //Process the next oscillator sample
         osc_low_out = osc_low.Process();
+
+        // TODO: add an overdrive stage to the kick
 
         //Get the next snare sample
         noise_out = noise.Process();
@@ -122,8 +134,6 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
 int main(void)
 {
     // Configure and Initialize the Daisy Seed
-    // These are separate to allow reconfiguration of any of the internal
-    // components before initialization.
     hw.Configure();
     hw.Init();
     hw.SetAudioBlockSize(4);
@@ -137,30 +147,33 @@ int main(void)
     //Initialize noise
     noise.Init();
 
-    //Initialize envelopes, this one's for the snare amplitude
+    // Initialize snare envelopes
     snareEnv.Init(samplerate);
     snareEnv.SetTime(ADENV_SEG_ATTACK, .01);
     snareEnv.SetTime(ADENV_SEG_DECAY, .2);
     snareEnv.SetMax(1);
     snareEnv.SetMin(0);
 
-    //This envelope will control the kickTrig oscillator's pitch
-    //Note that this envelope is much faster than the volume
+    // Initialize kick pitch envelope
     kickPitchEnv.Init(samplerate);
     kickPitchEnv.SetTime(ADENV_SEG_ATTACK, .01);
     kickPitchEnv.SetTime(ADENV_SEG_DECAY, .05);
 
-    //This one will control the kickTrig's volume
+    // Initialize kick volume envelope
     kickVolEnv.Init(samplerate);
     kickVolEnv.SetTime(ADENV_SEG_ATTACK, .01);
     kickVolEnv.SetMax(1);
     kickVolEnv.SetMin(0);
 	kickVolEnv.SetCurve(-20.f);
 
-    //Initialize the kickTrig and snare buttons
+    //Initialize the kick and snare triggers
     //The callback rate is samplerate / blocksize (48)
-    snareTrig.Init(hw.GetPin(SNARE_TRIG_PIN), samplerate / 48.f);
-    kickTrig.Init(hw.GetPin(KICK_TRIG_PIN), samplerate / 48.f);
+    kickTrig.Init(
+        D7,
+        GPIO::Mode::INPUT,
+        GPIO::Pull::PULLDOWN
+    );
+    btn.Init(hw.GetPin(BTN_PIN), samplerate / 48.f);
 
 	// Initialize ADCs
 	AdcChannelConfig adcConfig[numAdcChannels];
@@ -175,6 +188,10 @@ int main(void)
 	// Initialize the hw ADC
 	hw.adc.Init(adcConfig, numAdcChannels);
 	hw.adc.Start();
+
+    // Start USB serial logging
+    hw.StartLog(true);
+    hw.PrintLine("DrumDrum Module Ready");
 
     //Start calling the callback function
     hw.StartAudio(AudioCallback);

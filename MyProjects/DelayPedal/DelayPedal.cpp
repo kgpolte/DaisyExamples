a47a76b14391d132b8e02a4a1369288c16fd976d
kgpolte <120602839+kgpolte@users.noreply.github.com>
2024-06-15 19:19:09 -0700

    Update DelayPedal.cpp
    Improve pot reading to eliminate noisy modulation effects

--- MyProjects/DelayPedal/DelayPedal.cpp
+++ MyProjects/DelayPedal/DelayPedal.cpp
@@ -14,21 +14,24 @@ using namespace daisy;
 using namespace seed;
 
 static DaisySeed hw;
+
+// Dual Delay Lines
 static DelayLine<float, MAX_DELAY> del_l, del_r;
 
-// Analog Control Stuff
-const int num_pots = 6;
+
+// Analog Controls
+const int num_pots = 6;		// hardware variables
 const int pot_pins[num_pots] = {15,	16,	17,	18,	19,	20};
 const float pot_move_threshold = 0.01f;
 
-float lvl_time = 0.5f;
-float prev_lvl_time = 0.f;
+float lvl_time = 0.5f;		// Effect parameters
 float lvl_mix = 0.5f;
 float lvl_feedback = 0.5f;
 
-const int index_time = 0;
+const int index_time = 0; 	// AdcConfig indeces
 const int index_mix = 1;
 const int index_feedback = 2;
+
 
 // Digital Control Stuff
 
@@ -72,41 +75,56 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     }
 }
 
-void update() {
-	lvl_mix = hw.adc.GetFloat(index_mix);
-	lvl_feedback = hw.adc.GetFloat(index_feedback);
+// ------------------------------------------------------------------
 
-	float read_pot_time = hw.adc.GetFloat(index_time);
-	if (abs(read_pot_time - prev_lvl_time) > pot_move_threshold) {
-		del_l.SetDelay(MAX_DELAY * read_pot_time);
-		del_r.SetDelay(MAX_DELAY * read_pot_time);
-		prev_lvl_time = read_pot_time;
+void update() {
+	// Only update the control level if the pot has moved enough
+
+	// Time
+	const float reading_time = hw.adc.GetFloat(index_time);
+	if (abs(reading_time - lvl_time) > pot_move_threshold) {
+		del_l.SetDelay(MAX_DELAY * reading_time);
+		del_r.SetDelay(MAX_DELAY * reading_time);
+		lvl_time = reading_time;
+	}
+
+	// Mix
+	const float reading_mix = hw.adc.GetFloat(index_mix);
+	if (abs(reading_mix - lvl_mix) > pot_move_threshold) {
+		lvl_mix = reading_mix;
+	}
+
+	// Feedback
+	const float reading_feedback = hw.adc.GetFloat(index_feedback);
+	if (abs(reading_feedback - lvl_feedback) > pot_move_threshold) {
+		lvl_feedback = reading_feedback;
 	}
 }
 
+// ------------------------------------------------------------------
+
 int main(void)
 {
-    // initialize seed hardware and daisysp modules
+	// hardware config
     float sample_rate;
     hw.Configure();
     hw.Init();
     hw.SetAudioBlockSize(4);
     sample_rate = hw.AudioSampleRate();
 
-	// Configure the ADC
+	// ADC config and initialization
 	AdcChannelConfig adc_config[num_pots];
 	for (int i = 0; i < num_pots; i++) {
 		adc_config[i].InitSingle(hw.GetPin(pot_pins[i]));
 	}
-
 	hw.adc.Init(adc_config, num_pots);
 	hw.adc.Start();
 
-	// Initialize the delay lines
+	// delay line config
     del_l.Init();
 	del_r.Init();
 
-    // start callback
+	// start the audio callback
     hw.StartAudio(AudioCallback);
 
     while(1) {

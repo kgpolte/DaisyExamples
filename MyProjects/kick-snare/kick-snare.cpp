d92a939d53f59a2c2f48636d65268007f1b62c5f
kgpolte <120602839+kgpolte@users.noreply.github.com>
2024-07-28 16:50:03 -0700

    Update kick-snare.cpp
    Refactor bass drum class to accommodate new hardware needs for snare drum.

--- MyProjects/kick-snare/kick-snare.cpp
+++ MyProjects/kick-snare/kick-snare.cpp
@@ -5,48 +5,78 @@ using namespace daisy;
 using namespace daisy::seed;
 using namespace daisysp;
 
+// -------------------------------------------------------------
+// Hardware
+// -------------------------------------------------------------
+
+// Hardware objects
 DaisySeed hw;
 Led led1, led2;
 
 // Hardware pin definitions
-constexpr Pin PIN_TRIG      = D13;
-constexpr Pin PIN_BTN1      = D2;
-constexpr Pin PIN_BTN2      = D3;
-constexpr Pin PIN_LED1      = D26;
-constexpr Pin PIN_LED2      = D27;
-constexpr Pin PIN_FREQ      = A0;
-constexpr Pin PIN_DECAY     = A1;
-constexpr Pin PIN_VELOCITY  = A2;
-constexpr Pin PIN_FM        = A3;
-constexpr Pin PIN_DRIVE     = A4;
-constexpr Pin PIN_TONE      = A5;
+constexpr Pin PIN_KICK_TRIG     = D13;
+constexpr Pin PIN_SNARE_TRIG    = D14;
+
+constexpr Pin PIN_BTN1      	= D2;
+constexpr Pin PIN_BTN2      	= D3;
+
+constexpr Pin PIN_LED1      	= D26;
+constexpr Pin PIN_LED2      	= D27;
+
+constexpr Pin PIN_KICK_FREQ     = A0;
+constexpr Pin PIN_KICK_DECAY    = A1;
+constexpr Pin PIN_KICK_VEL 		= A2;
+constexpr Pin PIN_KICK_FM      	= A3;
+constexpr Pin PIN_KICK_TONE   	= A4;
+
+constexpr Pin PIN_SNARE_FREQ    = A5;
+constexpr Pin PIN_SNARE_DECAY   = A6;
+constexpr Pin PIN_SNARE_VEL 	= A9;
+constexpr Pin PIN_SNARE_BLEND   = A10;
+constexpr Pin PIN_SNARE_TONE   	= A11;
 
 // ADC indeces
-int FREQ_INDEX      = 0;
-int DECAY_INDEX     = 1;
-int VELOCITY_INDEX  = 2;
-int FM_INDEX        = 3;
-int DRIVE_INDEX     = 4;
-int TONE_INDEX      = 5;
+const int NUM_ADC_CHANNELS 	= 10;
+
+const int KICK_FREQ_INDEX   = 0;
+const int KICK_DECAY_INDEX  = 1;
+const int KICK_VEL_INDEX  	= 2;
+const int KICK_FM_INDEX     = 3;
+const int KICK_TONE_INDEX   = 4;
+
+const int SNARE_FREQ_INDEX  = 5;
+const int SNARE_DECAY_INDEX = 6;
+const int SNARE_VEL_INDEX   = 7;
+const int SNARE_BLEND_INDEX = 8;
+const int SNARE_TONE_INDEX  = 9;
 
 // -------------------------------------------------------------
-//
+// Sound Generator Class Definitions
 // -------------------------------------------------------------
 
 class BassDrum {
 private:
     // hardware objects
     Switch m_trigger, m_btn;
-    AnalogControl m_freqCtrl, m_decayCtrl, m_velocityCtrl, m_driveCtrl, m_fmCtrl, m_toneCtrl;
+    AnalogControl m_freqCtrl, m_decayCtrl, m_velocityCtrl, m_fmCtrl, m_toneCtrl;
 
     // Audio objects
     Oscillator m_osc;
     AdEnv m_ampEnv, m_pitchEnv;
-    Overdrive m_od;
     Svf m_svf;
 
-    // Variables
-    float m_FreqMin, m_FreqMax, m_FmMin, m_FmMax, m_DecayMin, m_DecayMax, m_DriveMin, m_DriveMax, m_cutoffMin, m_cutoffMax;
+    // Member variables
+    float
+        m_FreqMin,
+        m_FreqMax,
+        m_FmMin,
+        m_FmMax,
+        m_DecayMin,
+        m_DecayMax,
+        m_vel_min,
+        m_vel_max,
+        m_cutoffMin,
+        m_cutoffMax;
 
     // Functions
     void m_HandleTrigger() {
@@ -55,8 +85,10 @@ private:
         float decayCV = m_decayCtrl.Value();
         float velocityCV = m_velocityCtrl.Value();
         float fmCV = m_fmCtrl.Value();
-        float driveCV = m_driveCtrl.Value();
         float toneCV = m_toneCtrl.Value();
+
+        // Set the velocity (amplitude envelope max)
+        m_ampEnv.SetMax(m_vel_min + ((m_vel_max - m_vel_min) * velocityCV));
 
         // Set the volume decay time
         float decayTime = (decayCV * (m_DecayMax - m_DecayMin)) + m_DecayMin;
@@ -75,9 +107,6 @@ private:
         m_ampEnv.Trigger();
         m_pitchEnv.Trigger();
 
-        // Set the overdrive amount
-        m_od.SetDrive(m_DriveMin + (driveCV * (m_DriveMax - m_DriveMin)));
-
         // Set the LPF cutoff freqency
         m_svf.SetFreq(m_cutoffMin + (toneCV * (m_cutoffMax - m_cutoffMin)));
     }
@@ -94,7 +123,6 @@ public:
         int decayAdcIndex,
         int velocityAdcIndex,
         int fmAdcIndex,
-        int driveAdcIndex,
         int toneAdcIndex
     ) {
         // Initialize AnalogControls
@@ -102,18 +130,17 @@ public:
         m_decayCtrl.Init(hw.adc.GetPtr(decayAdcIndex), samplerate, true);
         m_velocityCtrl.Init(hw.adc.GetPtr(velocityAdcIndex), samplerate, true);
         m_fmCtrl.Init(hw.adc.GetPtr(fmAdcIndex), samplerate, true);
-        m_driveCtrl.Init(hw.adc.GetPtr(driveAdcIndex), samplerate);
         m_toneCtrl.Init(hw.adc.GetPtr(toneAdcIndex), samplerate);
 
-        // Set minimum/maximum control values
-        m_FreqMin = 40.f;
-        m_FreqMax = 100.f;
-        m_DecayMin = 0.25f;
-        m_DecayMax = 10.f;
-        m_FmMin = 50.f;
-        m_FmMax = 250.f;
-        m_DriveMin = 0.2f;
-        m_DriveMax = 0.5f;
+        // Define minimum/maximum control values
+        m_FreqMin   = 40.f;
+        m_FreqMax   = 100.f;
+        m_DecayMin  = 0.25f;
+        m_DecayMax  = 10.f;
+        m_FmMin     = 50.f;
+        m_FmMax     = 250.f;
+        m_vel_min   = 0.3f;
+        m_vel_max   = 1.f;
         m_cutoffMin = 40.f;
         m_cutoffMax = 400.f;
 
@@ -132,7 +159,8 @@ public:
         // m_osc.SetWaveform(Oscillator::WAVE_POLYBLEP_TRI);
         m_osc.SetAmp(1);
 
-        // Initizialize amplitdue envelope
+        // Initizialize amplitude envelope
+        // TODO: Try different curves
         m_ampEnv.Init(samplerate);
         m_ampEnv.SetTime(ADENV_SEG_ATTACK, .01);
         m_ampEnv.SetMax(1);
@@ -144,9 +172,6 @@ public:
         m_pitchEnv.Init(samplerate);
         m_pitchEnv.SetTime(ADENV_SEG_ATTACK, .01);
         m_pitchEnv.SetTime(ADENV_SEG_DECAY, .05);
-
-        // Initialize overdrive and filter
-        m_od.Init();
 
         // Initialize low pass filter
         m_svf.Init(samplerate);
@@ -164,7 +189,6 @@ public:
         m_decayCtrl.Process();
         m_velocityCtrl.Process();
         m_fmCtrl.Process();
-        m_driveCtrl.Process();
         m_toneCtrl.Process();
 
         // Check for rising/falling edges on digital inputs
@@ -175,32 +199,35 @@ public:
     }
 
     float Process() {
+        float oscAmp, oscOut, sigOut;
+
         m_osc.SetFreq(m_pitchEnv.Process());
-        float osc_amp = m_ampEnv.Process();
-        m_osc.SetAmp(osc_amp);
-        led1.Set(osc_amp);
 
-        float oscOut = m_osc.Process();
+        oscAmp = m_ampEnv.Process();
+        m_osc.SetAmp(oscAmp);
+        led1.Set(oscAmp);
 
-        // TODO: Find a better way to mix in overdrive
-        float odOut = m_od.Process(oscOut / 2);
-        
-        m_svf.Process(odOut);
-        float sig = m_svf.Low();
+        oscOut = m_osc.Process();
+        m_svf.Process(oscOut);
+        sigOut = m_svf.Low();
 
-        return sig;
+        return sigOut;
     }
 };
 
+class SnareDrum {
+
+};
+
 // -------------------------------------------------------------
-//
+//	Sound Generator Objects
 // -------------------------------------------------------------
 
 BassDrum kick;
 SnareDrum snare;
 
 // -------------------------------------------------------------
-//
+// Audio Callback
 // -------------------------------------------------------------
 
 void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
@@ -209,18 +236,18 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
 {
     kick.Update();
 
-    float sig;
+    float sigOut;
     for(size_t i = 0; i < size; i += 2)
     {
         led1.Update();
         led2.Update();
-        sig = kick.Process();
-        out[i] = out[i + 1] = sig;
+        sigOut = kick.Process();
+        out[i] = out[i + 1] = sigOut;
     }
 }
 
 // -------------------------------------------------------------
-//
+// Setup
 // -------------------------------------------------------------
 
 int main(void)
@@ -238,33 +265,40 @@ int main(void)
     led2.Set(1.f);
 
     // Configure and initialize ADCs
-    AdcChannelConfig adcConfig[5];
-    adcConfig[FREQ_INDEX].InitSingle(PIN_FREQ);
-    adcConfig[DECAY_INDEX].InitSingle(PIN_DECAY);
-    adcConfig[VELOCITY_INDEX].InitSingle(PIN_VELOCITY);
-    adcConfig[FM_INDEX].InitSingle(PIN_FM);
-    adcConfig[DRIVE_INDEX].InitSingle(PIN_DRIVE);
-    adcConfig[TONE_INDEX].InitSingle(PIN_TONE);
-    hw.adc.Init(adcConfig, 6);
+    AdcChannelConfig adcConfig[NUM_ADC_CHANNELS];
+
+    adcConfig[KICK_FREQ_INDEX].InitSingle(PIN_KICK_FREQ);
+    adcConfig[KICK_DECAY_INDEX].InitSingle(PIN_KICK_DECAY);
+    adcConfig[KICK_VEL_INDEX].InitSingle(PIN_KICK_VEL);
+    adcConfig[KICK_FM_INDEX].InitSingle(PIN_KICK_FM);
+    adcConfig[KICK_TONE_INDEX].InitSingle(PIN_KICK_TONE);
+
+	adcConfig[SNARE_FREQ_INDEX].InitSingle(PIN_SNARE_FREQ);
+    adcConfig[SNARE_DECAY_INDEX].InitSingle(PIN_SNARE_DECAY);
+    adcConfig[SNARE_VEL_INDEX].InitSingle(PIN_SNARE_VEL);
+    adcConfig[SNARE_BLEND_INDEX].InitSingle(PIN_SNARE_BLEND);
+    adcConfig[SNARE_TONE_INDEX].InitSingle(PIN_SNARE_TONE);
+    
+    hw.adc.Init(adcConfig, NUM_ADC_CHANNELS);
     hw.adc.Start();
 
     // Initialize the BassDrum object
     kick.Init(
         samplerate,
-        PIN_TRIG,
+        PIN_KICK_TRIG,
         PIN_BTN1,
-        FREQ_INDEX,
-        DECAY_INDEX,
-        VELOCITY_INDEX,
-        FM_INDEX,
-        DRIVE_INDEX,
-        TONE_INDEX
+        KICK_FREQ_INDEX,
+        KICK_DECAY_INDEX,
+        KICK_VEL_INDEX,
+        KICK_FM_INDEX,
+        KICK_TONE_INDEX
     );
 
 	// Initialize the SnareDrum object
 
-    //Start calling the callback function
+    // Start the callback function
     hw.StartAudio(AudioCallback);
 
+	// Loop forever
     for(;;) {}
 }

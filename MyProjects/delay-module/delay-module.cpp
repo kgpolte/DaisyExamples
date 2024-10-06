c32ef430e4e63b671921f89af4639b7eda93601f
kgpolte <120602839+kgpolte@users.noreply.github.com>
2024-10-06 10:12:49 -0700

    Update and build delay-module.cpp
    Initial build to test audio, delay line, and CV functionality

--- MyProjects/delay-module/delay-module.cpp
+++ MyProjects/delay-module/delay-module.cpp
@@ -1,25 +1,96 @@
-#include "daisy_seed.h"
 #include "daisysp.h"
+#include "daisy_seed.h"
 
-using namespace daisy;
+// Interleaved audio definitions
+#define LEFT (i)
+#define RIGHT (i + 1)
+
+// Set max delay time to 0.75 of samplerate.
+#define MAX_DELAY static_cast<size_t>(48000 * 0.75f)
+
 using namespace daisysp;
+using namespace daisy;
 
-DaisySeed hw;
+static DaisySeed hw;
+AnalogControl cv_0, cv_1, cv_2, cv_3;
+float time_factor, fdbk_amount, mix;
 
-void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+// Declare 2 DelayLines of MAX_DELAY number of floats.
+static DelayLine<float, MAX_DELAY> delay_l, delay_r;
+
+static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
+                          AudioHandle::InterleavingOutputBuffer out,
+                          size_t                                size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+    float feedback_l, feedback_r, del_out_l, del_out_r, sig_in_l, sig_in_r, sig_out_l, sig_out_r;
+
+    time_factor = cv_0.Process();
+    fdbk_amount = cv_1.Process();
+    mix = cv_2.Process();
+    cv_3.Process();
+
+    delay_l.SetDelay(MAX_DELAY * time_factor);
+    delay_r.SetDelay(MAX_DELAY * time_factor);
+
+    for(size_t i = 0; i < size; i += 2)
+    {
+        // Read from the inputs and delay line
+        sig_in_l = in[LEFT];
+        sig_in_r = in[RIGHT];
+        del_out_l = delay_l.Read();
+        del_out_r = delay_r.Read();
+
+        // Calculate output and feedback
+        sig_out_l  = del_out_l + sig_in_l;
+        feedback_l = (del_out_l * fdbk_amount) + sig_in_l;
+        sig_out_r  = del_out_r + sig_in_r;
+        feedback_r = (del_out_r * fdbk_amount) + sig_in_r;
+
+        // Write to the delay
+        delay_l.Write(feedback_l);
+        delay_r.Write(feedback_r);
+
+        // Set the outputs
+        out[LEFT]  = sig_out_l;
+        out[RIGHT] = sig_out_r;
+    }
 }
 
 int main(void)
 {
-	hw.Init();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
-	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    // initialize seed hardware and daisysp modules
+    float sample_rate;
+    hw.Configure();
+    hw.Init();
+    hw.SetAudioBlockSize(4);
+    sample_rate = hw.AudioSampleRate();
+
+	// initialize the delay lines
+    delay_l.Init();
+    delay_l.SetDelay(MAX_DELAY);
+    delay_r.Init();
+    delay_r.SetDelay(MAX_DELAY);
+
+	// initialize the ADC
+	const int num_adc_channels = 4;
+	AdcChannelConfig adcConfig[num_adc_channels];
+	adcConfig[0].InitSingle(seed::A0);
+    adcConfig[1].InitSingle(seed::A1);
+    adcConfig[2].InitSingle(seed::A2);
+    adcConfig[3].InitSingle(seed::A3);
+	hw.adc.Init(adcConfig, num_adc_channels);
+	hw.adc.Start();
+
+	// initialize the AnalogControls
+	cv_0.Init(hw.adc.GetPtr(0), sample_rate, true, false, 0.01f);
+    cv_1.Init(hw.adc.GetPtr(1), sample_rate, true, false, 0.01f);
+    cv_2.Init(hw.adc.GetPtr(2), sample_rate, true, false, 0.01f);
+    cv_3.Init(hw.adc.GetPtr(3), sample_rate, true, false, 0.01f);
+
+    // start callback
+    hw.StartAudio(AudioCallback);
+
+    while(1) {
+		
+	}
 }

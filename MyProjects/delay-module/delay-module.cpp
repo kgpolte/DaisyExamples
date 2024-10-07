ed7bf951860923d116ae6d2a64fd6cadd3fade25
kgpolte <120602839+kgpolte@users.noreply.github.com>
2024-10-06 22:40:21 -0700

    Update delay-module.cpp
    Move initializations and main loop code into separate functions.

--- MyProjects/delay-module/delay-module.cpp
+++ MyProjects/delay-module/delay-module.cpp
@@ -12,12 +12,15 @@
 #define LEFT (i)
 #define RIGHT (i + 1)
 
-// Set max delay time to 1 second
+// Set max delay time to 2 seconds and min delay time to 0.0001 second
 #define MAX_DELAY static_cast<size_t>(96000 * 2.0f)
+#define MIN_DELAY static_cast<size_t>(96000 * 0.0001f)
 
+// namespaces
 using namespace daisysp;
 using namespace daisy;
 
+// Daisy modules
 static DaisySeed hw;
 static AnalogControl cv_0, cv_1, cv_2, cv_3;
 static GateIn clock, hold;
@@ -25,11 +28,97 @@ static Led led_1_b, led_1_r, led_2_b, led_2_r;
 static CrossFade cross_fader;
 static Switch switch_1, switch_2;
 
+// Global variables
 bool clock_sync = false;
 
-// Declare 2 DelayLines of MAX_DELAY number of floats.
+// 2 DelayLines of MAX_DELAY number of floats.
 static DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS delay_l;
 static DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS delay_r;
+
+
+// --------------------------------------------------------------------
+
+static void InitAnalogControls(float sample_rate)
+{
+    // initialize the hardware ADC
+    const int num_adc_channels = 4;
+	AdcChannelConfig adcConfig[num_adc_channels];
+	adcConfig[0].InitSingle(seed::A0);
+    adcConfig[1].InitSingle(seed::A1);
+    adcConfig[2].InitSingle(seed::A2);
+    adcConfig[3].InitSingle(seed::A3);
+	hw.adc.Init(adcConfig, num_adc_channels);
+	hw.adc.Start();
+
+    // initialize the AnalogControl objects
+	cv_0.Init(hw.adc.GetPtr(0), sample_rate, true, false, 0.1f);
+    cv_1.Init(hw.adc.GetPtr(1), sample_rate, true, false, 0.1f);
+    cv_2.Init(hw.adc.GetPtr(2), sample_rate, true, false, 0.1f);
+    cv_3.Init(hw.adc.GetPtr(3), sample_rate, true, false, 0.1f);
+}
+
+static void InitDac()
+{
+	DacHandle::Config dac_cfg;
+	dac_cfg.bitdepth = DacHandle::BitDepth::BITS_12;
+	dac_cfg.buff_state = DacHandle::BufferState::ENABLED;
+	dac_cfg.mode = DacHandle::Mode::POLLING;
+	dac_cfg.chn = DacHandle::Channel::BOTH;
+	hw.dac.Init(dac_cfg);
+	hw.dac.WriteValue(DacHandle::Channel::ONE, 0);
+	hw.dac.WriteValue(DacHandle::Channel::TWO, 0);
+}
+
+static void InitDspModules()
+{
+    // initialize the delay lines
+    delay_l.Init();
+    delay_r.Init();
+
+    // initialize the cross_fader (dry/wet control)
+    cross_fader.Init();
+    cross_fader.SetCurve(CROSSFADE_LIN);
+}
+
+static void InitGpio()
+{
+    // initialize the gate inputs
+    clock.Init(seed::D26);
+    hold.Init(seed::D27);
+
+    // initialize the push button switches
+    switch_1.Init(seed::D12, 1000.0f);
+    switch_2.Init(seed::D11, 1000.0f);
+
+    // initialize the LEDs
+    led_1_b.Init(seed::D29, false);
+    led_1_r.Init(seed::D30, false);
+    led_2_b.Init(seed::D8, false);
+    led_2_r.Init(seed::D7, false);
+}
+
+static void ProcessSwitches()
+{
+    switch_1.Debounce();
+    switch_2.Debounce();
+
+    if (switch_1.RisingEdge()) {
+        clock_sync = !clock_sync;
+        if (!clock_sync) {
+            led_1_b.Set(0.0f);
+            led_1_r.Set(0.0f);
+        }
+    }
+}
+
+static void UpdateLeds()
+{
+    led_1_b.Update();
+    led_1_r.Update();
+    led_2_b.Update();
+    led_2_r.Update();
+}
+
 static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
                           AudioHandle::InterleavingOutputBuffer out,
                           size_t                                size)
@@ -90,68 +179,17 @@ int main(void)
     hw.SetAudioBlockSize(4);
     sample_rate = hw.AudioSampleRate();
 
-	// initialize the delay lines
-    delay_l.Init();
-    delay_r.Init();
-
-    // initialize DAC outputs
-	DacHandle::Config dac_cfg;
-	dac_cfg.bitdepth = DacHandle::BitDepth::BITS_12;
-	dac_cfg.buff_state = DacHandle::BufferState::ENABLED;
-	dac_cfg.mode = DacHandle::Mode::POLLING;
-	dac_cfg.chn = DacHandle::Channel::BOTH;
-	hw.dac.Init(dac_cfg);
-	hw.dac.WriteValue(DacHandle::Channel::ONE, 1028); // CV0
-	hw.dac.WriteValue(DacHandle::Channel::TWO, 3084); // CV1
-
-	// initialize the ADC
-	const int num_adc_channels = 4;
-	AdcChannelConfig adcConfig[num_adc_channels];
-	adcConfig[0].InitSingle(seed::A0);
-    adcConfig[1].InitSingle(seed::A1);
-    adcConfig[2].InitSingle(seed::A2);
-    adcConfig[3].InitSingle(seed::A3);
-	hw.adc.Init(adcConfig, num_adc_channels);
-	hw.adc.Start();
-
-	// initialize the AnalogControls
-	cv_0.Init(hw.adc.GetPtr(0), sample_rate, true, false, 0.1f);
-    cv_1.Init(hw.adc.GetPtr(1), sample_rate, true, false, 0.1f);
-    cv_2.Init(hw.adc.GetPtr(2), sample_rate, true, false, 0.1f);
-    cv_3.Init(hw.adc.GetPtr(3), sample_rate, true, false, 0.1f);
-
-    // initialize the gate inputs
-    clock.Init(seed::D26);
-    hold.Init(seed::D27);
-
-    // initialize the push button switches
-    switch_1.Init(seed::D12, 1000.0f);
-    switch_2.Init(seed::D11, 1000.0f);
-
-    // initialize the LEDs
-    led_1_b.Init(seed::D29, false);
-    led_1_r.Init(seed::D30, false);
-    led_2_b.Init(seed::D8, false);
-    led_2_r.Init(seed::D7, false);
-
-    // initialize the cross_fader (dry/wet control)
-    cross_fader.Init();
-    cross_fader.SetCurve(CROSSFADE_LIN);
+    // other initializations
+    InitAnalogControls(sample_rate);
+    InitDac();
+    InitGpio();
+    InitDspModules();
 
     // start the audio callback
     hw.StartAudio(AudioCallback);
 
     while(1) {
-        switch_1.Debounce();
-        switch_2.Debounce();
-
-        if (switch_1.RisingEdge()) {
-            clock_sync = !clock_sync;
-            if (!clock_sync) {
-                led_1_b.Set(0.0f);
-                led_1_r.Set(0.0f);
-            }
-        }
+        ProcessSwitches();
 
         if (clock_sync) {
             if (clock.State()) {
@@ -161,13 +199,8 @@ int main(void)
                 led_1_b.Set(0.3f);
                 led_1_r.Set(0.3f);
             }
-        } else {
-            
         }
-
-        led_1_b.Update();
-        led_1_r.Update();
-        led_2_b.Update();
-        led_2_r.Update();
+        
+        UpdateLeds();
 	}
 }
